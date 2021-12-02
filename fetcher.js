8483bad6c7d0415eb7d35c379d7afb51a1a517b5
Nick Rusin <woundculture@gmail.com>
2021-12-02 03:35:01 +0000

    formatting

--- fetcher.js
+++ fetcher.js
@@ -9,17 +9,22 @@ const net = require('net');
 
 // -- Begin Here
 const fetcher = function(URL, filePath) {
+
   // -- set command line arguments at index 2 and 3 to 
   // -- URL and filePath variables respectively
   URL = process.argv[2];
   filePath = `${process.argv[3]}`;
+
   // -- create connection to host
   const conn = net.createConnection({
+
     // -- host can't start with http. substring removes protocol characters.
     host: `${URL.substring(7)}`,
     port: 80,
   });
+
   conn.setEncoding('UTF8');
+
   // -- Request the goodies
   conn.on('connect', () => {
     console.log(`Connected to server!`);
@@ -27,6 +32,7 @@ const fetcher = function(URL, filePath) {
     conn.write(`Host: ${URL.substring(7)}\r\n`);
     conn.write(`\r\n`);
   });
+
   // -- Write recieved goods (a+ to append to existing, or creates a file)
   conn.on('data', (data) => {
     fs.writeFile(`${filePath}`, data, err => {
@@ -38,6 +44,7 @@ const fetcher = function(URL, filePath) {
     })
     conn.end();
   })
+  
 ;}
 
 fetcher();
